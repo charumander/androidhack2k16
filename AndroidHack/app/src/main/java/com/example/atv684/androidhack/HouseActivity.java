5a268e852d31def0450b7e3f4e79150ff6bcb81e
Oleg Godovykh <ogodovykh@eastbanctech.com>
2016-08-27 12:08:35 -0400

    add empty house activity

---
+++ AndroidHack/app/src/main/java/com/example/atv684/androidhack/HouseActivity.java
@@ -0,0 +1,13 @@
+package com.example.atv684.androidhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HouseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house);
+    }
+}
