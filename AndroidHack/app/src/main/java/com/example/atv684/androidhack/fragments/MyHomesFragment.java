4a10fb74542ddc0ceab40e5ddc518ea906e2de14
Oleg Godovykh <ogodovykh@eastbanctech.com>
2016-08-27 14:54:13 -0400

    move from my homes to house activity

--- AndroidHack/app/src/main/java/com/example/atv684/androidhack/fragments/MyHomesFragment.java
+++ AndroidHack/app/src/main/java/com/example/atv684/androidhack/fragments/MyHomesFragment.java
@@ -1,45 +1,30 @@
 package com.example.atv684.androidhack.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.atv684.androidhack.HouseActivity;
 import com.example.atv684.androidhack.HouseListAdapter.HouseListAdapter;
 import com.example.atv684.androidhack.R;
 import com.example.atv684.androidhack.objects.House;
 import com.example.atv684.androidhack.objects.HouseTypePref;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * Created by atv684 on 8/27/16.
  */
 public class MyHomesFragment extends ListFragment{
-
-    public ListView listView;
-
     public HouseListAdapter adapter;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.house_list_fragment, container, false);
-        return view;
+        return inflater.inflate(R.layout.house_list_fragment, container, false);
     }
 
 
@@ -47,30 +32,18 @@ public class MyHomesFragment extends ListFragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
         ArrayList<House> houses = new ArrayList<House>();
 
         houses.add(new House("Name", "Description", "zip", 500000, null, 3, 2, HouseTypePref.Apartment.toString()));
 
         adapter = new HouseListAdapter(getContext(), houses);
         setListAdapter(adapter);
-        //getListView().setOnItemClickListener(this);
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        // TODO implement some logic
+        HouseActivity.showHouse(getContext(), (House)adapter.getItem(position));
     }
-//
-//    @Override
-//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//    }
 }
 
 
