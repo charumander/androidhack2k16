512e3e6cba2dac38dd66cc47f0867d77204a76af
Oleg Godovykh <ogodovykh@eastbanctech.com>
2016-08-27 13:17:05 -0400

    set up house image loading

--- AndroidHack/app/src/main/java/com/example/atv684/androidhack/HouseActivity.java
+++ AndroidHack/app/src/main/java/com/example/atv684/androidhack/HouseActivity.java
@@ -1,13 +1,100 @@
 package com.example.atv684.androidhack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.atv684.androidhack.objects.House;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class HouseActivity extends AppCompatActivity {
+    public static final String HOUSE_EXTRA = "HOUSE_EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!getIntent().hasExtra(HOUSE_EXTRA)) {
+            finish();
+            return;
+        }
+        final House house = (House) getIntent().getSerializableExtra(HOUSE_EXTRA);
+        if (house == null) {
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_house);
+
+        final ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        final CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+
+        final HouseAdapter adapter = new HouseAdapter(getSupportFragmentManager(), house);
+
+        pager.setAdapter(adapter);
+        indicator.setViewPager(pager);
+    }
+
+
+    private static class HouseAdapter extends FragmentStatePagerAdapter {
+        private House house;
+
+        public HouseAdapter(FragmentManager fm, final House house) {
+            super(fm);
+            this.house = house;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ImageFragment.newInstance(house.getHouseImages().get(position));
+        }
+
+        @Override
+        public int getCount() {
+            final List<String> images = house.getHouseImages();
+            return (images != null) ? images.size() : 0;
+        }
+    }
+
+    public static class ImageFragment extends Fragment {
+        private static final String IMAGE_EXTRA = "IMAGE_EXTRA";
+
+        public static ImageFragment newInstance(String imageUrl) {
+            final ImageFragment fragment = new ImageFragment();
+            final Bundle arguments = new Bundle();
+            arguments.putString(IMAGE_EXTRA, imageUrl);
+            fragment.setArguments(arguments);
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            if (container == null) {
+                return null;
+            }
+
+            final Context context = container.getContext();
+            final ImageView view = new ImageView(context);
+            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+            final String url = getArguments().getString(IMAGE_EXTRA, null);
+            Picasso.with(context).load(url).into(view);
+
+            return view;
+        }
     }
 }
